8d11648f2a2d70ba3e7d447311c1dc751105158b
RastGame <rastgame1@gmail.com>
2023-04-19 23:44:00 +0300

    Cmake

---
+++ build/CMakeFiles/Xandria.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Xandria.dir/src/main.cpp.o"
+  "CMakeFiles/Xandria.dir/src/main.cpp.o.d"
+  "Xandria"
+  "Xandria.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Xandria.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
